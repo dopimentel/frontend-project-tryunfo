9e801d5bcab61df322e4d5dad498962303f5c74c
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-09-05 04:32:49 -0300

    Requisito 2 - parcial

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,26 +1,83 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Input from './Input';
 
 class Form extends Component {
   render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      hasTrunfo,
+      isSaveButtonDisabled,
+      onInputChange,
+      onSaveButtonClick,
+    } = this.props;
+
     return (
       <form>
-        <Input name="name" type="text" dataTestId="name-input" />
+        <Input
+          name={ cardName }
+          type="text"
+          dataTestId="name-input"
+          onInputChange={ onInputChange }
+        />
         <label htmlFor="description">
-          <textarea id="description" data-testid="description-input" />
+          <textarea
+            value={ cardDescription }
+            id="description"
+            data-testid="description-input"
+            onChange={ onInputChange }
+          />
         </label>
-        <Input name="attr1" type="number" dataTestId="attr1-input" />
-        <Input name="attr2" type="number" dataTestId="attr2-input" />
-        <Input name="attr3" type="number" dataTestId="attr3-input" />
-        <Input name="image" type="text" dataTestId="image-input" />
+        <Input
+          name={ cardAttr1 }
+          type="number"
+          dataTestId="attr1-input"
+          onInputChange={ onInputChange }
+        />
+        <Input
+          name={ cardAttr2 }
+          type="number"
+          dataTestId="attr2-input"
+          onInputChange={ onInputChange }
+        />
+        <Input
+          name={ cardAttr3 }
+          type="number"
+          dataTestId="attr3-input"
+          onInputChange={ onInputChange }
+        />
+        <Input
+          name={ cardImage }
+          type="text"
+          dataTestId="image-input"
+          onInputChange={ onInputChange }
+        />
         <label htmlFor="rare">
-          <select data-testid="rare-input" id="rare">
+          <select
+            data-testid="rare-input"
+            id="rare"
+            value={ cardRare }
+            onChange={ onInputChange }
+          >
             <option>normal</option>
             <option>raro</option>
             <option>muito raro</option>
           </select>
         </label>
-        <Input name="trunfo" type="checkbox" dataTestId="trunfo-input" />
+        <Input
+          name="trunfo"
+          type="checkbox"
+          checked={ cardTrunfo }
+          dataTestId="trunfo-input"
+          onChange={ onInputChange }
+        />
         <button type="button" data-testid="save-button">
           Salvar
         </button>
@@ -29,4 +86,18 @@ class Form extends Component {
   }
 }
 
+Form.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.string.isRequired,
+  cardAttr2: PropTypes.string.isRequired,
+  cardAttr3: PropTypes.string.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+  hasTrunfo: PropTypes.bool.isRequired,
+  isSaveButtonDisabled: PropTypes.bool.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+  onSaveButtonClick: PropTypes.func.isRequired,
+};
 export default Form;
--- src/components/Input.js
+++ src/components/Input.js
@@ -3,10 +3,16 @@ import PropTypes from 'prop-types';
 
 class Input extends Component {
   render() {
-    const { name, type, dataTestId } = this.props;
+    const { name, type, dataTestId, onInputChange } = this.props;
     return (
       <label htmlFor={ name }>
-        <input type={ type } data-testid={ dataTestId } id={ name } />
+        <input
+          type={ type }
+          data-testid={ dataTestId }
+          id={ name }
+          value={ name }
+          onChange={ onInputChange }
+        />
       </label>
     );
   }
@@ -16,5 +22,6 @@ Input.propTypes = {
   name: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
   dataTestId: PropTypes.string.isRequired,
+  onInputChange: PropTypes.func.isRequired,
 };
 export default Input;
